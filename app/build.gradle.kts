00ae078b171134cfb3d42e4ffb415139e5d47d1f
Альбина <albiondoris@gmail.com>
2026-09-13 22:48:13 +0300

    Add application setup and welcome message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-    id("java")
-    application
+    id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
