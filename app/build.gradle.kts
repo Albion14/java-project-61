f448b171f6d4692ccf7225ffdc224134e7a767ff
Альбина <albiondoris@gmail.com>
2026-09-14 13:48:27 +0300

    Changing permission of gradlew

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,16 +25,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
